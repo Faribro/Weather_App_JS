b380375130e31e166c679ff558a967a8232879ea
Farid Sayyed <49672595+Faribro@users.noreply.github.com>
2022-12-16 00:23:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,9 @@ function weatherDetails(info) {
     } else if(id === 404){
       wIcon.src = "cloudy-day-1.svg";
       console.log("I am faulty, FIX ME!");
+    } else {
+      wIcon.src = "cloudy-day-1.svg";
+      console.log("I am also fulty);
     }
 
     weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
